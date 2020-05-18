8e1cfeaca5c2d1af07262ac318844cbfea04bfe7
Jan D’Hollander <jan.dhollander84@gmail.com>
2020-05-18 15:25:12 +0200

    cleanup & add gradientColors

--- src/js/figma/colors.js
+++ src/js/figma/colors.js
@@ -1,7 +1,8 @@
 export default function() {
   // eslint-disable-next-line
   const colorStyles = figma.getLocalPaintStyles();
-  const hexValueAndName = []; // array of hex values and their names
+  const colors = []; // array of hex values and their names
+  const gradientColors = [];
 
   function rgbToHex(int) {
     let hex = Number(int).toString(16);
@@ -18,18 +19,32 @@ export default function() {
     return `#${red}${green}${blue}`;
   }
 
+  function makeRgb(color) {
+    const r = Math.round(255 * color.r);
+    const g = Math.round(255 * color.g);
+    const b = Math.round(255 * color.b);
+    return {r, g, b}
+  }
+
   colorStyles.forEach((style) => {
     /* Only work with solid colors */
     if (style.paints[0].color){
       const { name } = style;
-      const r = Math.round(255 * style.paints[0].color.r);
-      const g = Math.round(255 * style.paints[0].color.g);
-      const b = Math.round(255 * style.paints[0].color.b);
+      const {r, g, b} = makeRgb(style.paints[0].color);
       const value = makeHex(r, g, b);
       const result = { name, value };
-      hexValueAndName.push(result);  
+      colors.push(result);  
+    } 
+     /* Add gradients as a suggestion */
+    else if(style.paints[0].gradientStops.length > 0 ){
+      style.paints[0].gradientStops.forEach(stop => {
+        const {r, g, b} = makeRgb(stop.color);
+        const value = makeHex(r, g, b);
+        gradientColors.push(value);
+      })
     }
   });
 
-  return hexValueAndName;
+
+  return {colors, gradientColors};
 }
